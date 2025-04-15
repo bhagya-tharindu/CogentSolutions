473273af88aa7d202dcddad18515b89fa8bb775a
Asitha Nilanga <bhagya.tharindu1@gmail.com>
2025-04-15 21:10:28 +0530

    favicon icon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
